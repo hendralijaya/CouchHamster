d0830430975bab0c68bce90ab21c3c5c3c92bc05
Hendra Lijaya <hendralijaya@gmail.com>
2024-05-17 23:31:32 +0700

    Initial Commit

---
+++ CouchHamsterNew/CouchHamsterNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouchHamsterNewApp.swift
+//  CouchHamsterNew
+//
+//  Created by hendra on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CouchHamsterNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
